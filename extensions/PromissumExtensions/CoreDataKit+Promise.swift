4de5c13cd3bd94d643370692b0ab2eb6f3c5141f
Tom Lokhorst <tom@lokhorst.eu>
2015-01-06 23:03:47 +0100

    Added CoreDataKit extension

---
+++ extensions/PromissumExtensions/CoreDataKit+Promise.swift
@@ -0,0 +1,56 @@
+//
+//  CoreDataKit+Promise.swift
+//  PromissumExtensions
+//
+//  Created by Mathijs Kadijk on 2014-10-27.
+//  Copyright (c) 2014 Mathijs Kadijk. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import CoreDataKit
+import Promissum
+
+extension Result {
+  func toPromise() -> Promise<T> {
+    switch self {
+    case let .Success(boxedObject):
+      return Promise<T>(value: boxedObject())
+
+    case let .Failure(error):
+      return Promise<T>(error: error)
+    }
+  }
+}
+
+extension CoreDataKit {
+  public class func performBlockOnBackgroundContextPromise(block: PerformBlock) -> Promise<CommitAction> {
+    return sharedStack!.performBlockOnBackgroundContextPromise(block)
+  }
+}
+
+extension CoreDataStack {
+  public func performBlockOnBackgroundContextPromise(block: PerformBlock) -> Promise<CommitAction> {
+    return rootContext.createChildContext().performBlockPromise(block)
+  }
+}
+
+extension NSManagedObjectContext {
+  public func performBlockPromise(block: PerformBlock) -> Promise<CommitAction> {
+    let promiseSource = PromiseSource<CommitAction>()
+
+    performBlock(block) { result in
+      dispatch_async(dispatch_get_main_queue()) {
+        switch result {
+        case let .Success(commitAction):
+          promiseSource.resolve(commitAction())
+
+        case let .Failure(error):
+          promiseSource.reject(error)
+        }
+      }
+    }
+
+    return promiseSource.promise
+  }
+}
