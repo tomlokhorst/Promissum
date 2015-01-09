50d9a84c98f399ad80ae9de7a2eea132a0ea4350
Tom Lokhorst <tom@lokhorst.eu>
2015-01-09 09:03:40 +0100

    Added TinyNetworking+Promise

---
+++ extensions/PromissumExtensions/TinyNetworking+Promise.swift
@@ -0,0 +1,54 @@
+//
+//  TinyNetworking+Promise.swift
+//  PromissumExtensions
+//
+//  Created by Tom Lokhorst on 2015-01-08.
+//  Copyright (c) 2015 Tom Lokhorst. All rights reserved.
+//
+
+import Foundation
+import Promissum
+
+public let TinyNetworkingPromiseErrorDomain = "com.nonstrict.promissum.tiny-networking"
+
+public let TinyNetworkingPromiseReasonKey = "reason"
+public let TinyNetworkingPromiseDataKey = "data"
+
+public func apiRequestPromise<A>(modifyRequest: NSMutableURLRequest -> (), baseURL: NSURL, resource: Resource<A>) -> Promise<A> {
+  let source = PromiseSource<A>()
+
+  func onFailure(reason: Reason, data: NSData?) {
+    var userInfo: [NSObject: AnyObject] = [
+      TinyNetworkingPromiseReasonKey: Box(reason),
+    ]
+    if let data = data {
+      userInfo[TinyNetworkingPromiseDataKey] = data
+    }
+
+    source.reject(NSError(domain: TinyNetworkingPromiseErrorDomain, code: -1, userInfo: userInfo))
+  }
+
+  apiRequest(modifyRequest, baseURL, resource, onFailure, source.resolve)
+
+  return source.promise
+}
+
+public class Box<T> {
+  public let unbox: T
+  public init(_ value: T) { self.unbox = value }
+}
+
+extension Reason: Printable {
+  public var description: String {
+    switch self {
+    case .CouldNotParseJSON:
+      return "CouldNotParseJSON"
+    case .NoData:
+      return "NoData"
+    case let .NoSuccessStatusCode(statusCode):
+      return "NoSuccessStatusCode(\(statusCode))"
+    case let .Other(error):
+      return "Other(\(error))"
+    }
+  }
+}
---
+++ extensions/PromissumExtensionsTests/TinyNetworkingPromiseTests.swift
@@ -0,0 +1,49 @@
+//
+//  TinyNetworkingPromiseTests.swift
+//  PromissumExtensions
+//
+//  Created by Tom Lokhorst on 2015-01-09.
+//  Copyright (c) 2015 Tom Lokhorst. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import Promissum
+import PromissumExtensions
+
+class TinyNetworkingPromiseTests: XCTestCase {
+
+  func testAlamofire() {
+    let expectation = expectationWithDescription("Request not completed")
+
+    let baseURL = NSURL(string: "https://api.github.com")!
+    let repoResource = Resource(
+      path: "/repos/tomlokhorst/Promissum",
+      method: .GET,
+      requestBody: nil,
+      headers: [:],
+      parse: decodeJSON)
+
+
+    // Start the asynchroneous downloading of a large file, passing in the cancellation token
+    apiRequestPromise({ _ in }, baseURL, repoResource)
+      .then { json in
+        let name_ = json["name"] as? String
+        if name_ == "Promissum" {
+          expectation.fulfill()
+        }
+      }
+      .catch { e in
+        if e.domain == TinyNetworkingPromiseErrorDomain {
+          if let reason = e.userInfo?[TinyNetworkingPromiseReasonKey] as? Box<Reason> {
+            println(reason.unbox)
+            return
+          }
+        }
+        println(e)
+      }
+
+    // Wait for 1 second for the download to be cancelled
+    waitForExpectationsWithTimeout(5.0, handler: nil)
+  }
+}
