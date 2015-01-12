504faa0511e96742a06acdcdc6d7780feb76c9e7
Tom Lokhorst <tom@lokhorst.eu>
2015-01-12 20:52:24 +0100

    Added extension methods for UIAlertView and UIActionSheet.

--- extensions/PromissumExtensions/UIKit+Promise.swift
+++ extensions/PromissumExtensions/UIKit+Promise.swift
@@ -84,3 +84,142 @@ extension UIViewController {
     return source.promise
   }
 }
+
+var associatedObjectHandle: UInt8 = 0
+let associationPolicy = objc_AssociationPolicy(OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+
+// UIAlertView is deprecated per iOS 8, however this extension is here for convenience
+extension UIAlertView {
+  var strongDelegate: AlertViewDelegate? {
+    get {
+      return (objc_getAssociatedObject(self, &associatedObjectHandle) as AlertViewDelegate)
+    }
+    set {
+      objc_setAssociatedObject(self, &associatedObjectHandle, newValue, associationPolicy)
+    }
+  }
+
+  public func showPromise() -> Promise<Int> {
+    let source = PromiseSource<Int>()
+    let originalDelegate = self.delegate as? UIAlertViewDelegate
+
+    self.delegate = AlertViewDelegate(source: source, alertView: self, originalDelegate: originalDelegate)
+    self.show()
+
+    return source.promise
+  }
+
+  internal class AlertViewDelegate: NSObject, UIAlertViewDelegate {
+    let source: PromiseSource<Int>
+    let alertView: UIAlertView
+    let originalDelegate: UIAlertViewDelegate?
+
+    init(source: PromiseSource<Int>, alertView: UIAlertView, originalDelegate: UIAlertViewDelegate?) {
+      self.source = source
+      self.alertView = alertView
+      self.originalDelegate = originalDelegate
+
+      super.init()
+
+      self.alertView.strongDelegate = self
+    }
+
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+      originalDelegate?.alertView?(alertView, clickedButtonAtIndex: buttonIndex)
+
+      source.resolve(buttonIndex)
+
+      self.alertView.strongDelegate = nil
+    }
+
+    func alertViewCancel(alertView: UIAlertView) {
+      originalDelegate?.alertViewCancel?(alertView)
+    }
+
+    func willPresentAlertView(alertView: UIAlertView) {
+      originalDelegate?.willPresentAlertView?(alertView)
+    }
+
+    func didPresentAlertView(alertView: UIAlertView) {
+      originalDelegate?.didPresentAlertView?(alertView)
+    }
+
+    func alertView(alertView: UIAlertView, willDismissWithButtonIndex buttonIndex: Int) {
+      originalDelegate?.alertView?(alertView, willDismissWithButtonIndex: buttonIndex)
+    }
+
+    func alertView(alertView: UIAlertView, didDismissWithButtonIndex buttonIndex: Int) {
+      originalDelegate?.alertView?(alertView, didDismissWithButtonIndex: buttonIndex)
+    }
+
+    func alertViewShouldEnableFirstOtherButton(alertView: UIAlertView) -> Bool {
+      return originalDelegate?.alertViewShouldEnableFirstOtherButton?(alertView) ?? false
+    }
+  }
+}
+
+// UIAlertView is deprecated per iOS 8, however this extension is here for convenience
+extension UIActionSheet {
+  var strongDelegate: ActionSheetDelegate? {
+    get {
+      return (objc_getAssociatedObject(self, &associatedObjectHandle) as ActionSheetDelegate)
+    }
+    set {
+      objc_setAssociatedObject(self, &associatedObjectHandle, newValue, associationPolicy)
+    }
+  }
+
+  public func showInViewPromise(view: UIView!) -> Promise<Int> {
+    let source = PromiseSource<Int>()
+    let originalDelegate = self.delegate
+
+    self.delegate = ActionSheetDelegate(source: source, actionSheet: self, originalDelegate: originalDelegate)
+    self.showInView(view)
+
+    return source.promise
+  }
+
+  internal class ActionSheetDelegate: NSObject, UIActionSheetDelegate {
+    let source: PromiseSource<Int>
+    let actionSheet: UIActionSheet
+    let originalDelegate: UIActionSheetDelegate?
+
+    init(source: PromiseSource<Int>, actionSheet: UIActionSheet, originalDelegate: UIActionSheetDelegate?) {
+      self.source = source
+      self.actionSheet = actionSheet
+      self.originalDelegate = originalDelegate
+
+      super.init()
+
+      self.actionSheet.strongDelegate = self
+    }
+
+    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+      originalDelegate?.actionSheet?(actionSheet, clickedButtonAtIndex: buttonIndex)
+
+      source.resolve(buttonIndex)
+
+      self.actionSheet.strongDelegate = nil
+    }
+
+    func actionSheetCancel(actionSheet: UIActionSheet) {
+      originalDelegate?.actionSheetCancel?(actionSheet)
+    }
+
+    func willPresentActionSheet(actionSheet: UIActionSheet) {
+      originalDelegate?.willPresentActionSheet?(actionSheet)
+    }
+
+    func didPresentActionSheet(actionSheet: UIActionSheet) {
+      originalDelegate?.didPresentActionSheet?(actionSheet)
+    }
+
+    func actionSheet(actionSheet: UIActionSheet, willDismissWithButtonIndex buttonIndex: Int) {
+      originalDelegate?.actionSheet?(actionSheet, willDismissWithButtonIndex: buttonIndex)
+    }
+
+    func actionSheet(actionSheet: UIActionSheet, didDismissWithButtonIndex buttonIndex: Int) {
+      originalDelegate?.actionSheet?(actionSheet, didDismissWithButtonIndex: buttonIndex)
+    }
+  }
+}
