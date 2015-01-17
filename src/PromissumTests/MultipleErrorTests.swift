407ac88d3d4024c359bd2c42063357b8f55968ae
Tom Lokhorst <tom@lokhorst.eu>
2015-01-17 20:08:23 +0100

    Added flatMap tests

--- src/PromissumTests/MultipleErrorTests.swift
+++ src/PromissumTests/MultipleErrorTests.swift
@@ -87,6 +87,50 @@ class MultipleErrorTests: XCTestCase {
     XCTAssert(calls == 2, "Calls should be 2")
   }
 
+  func testValueFlatMap3() {
+    var calls = 0
+
+    let source1 = PromiseSource<Int>()
+    let p1 = source1.promise
+    let source2 = PromiseSource<Int>()
+    let p2 = source2.promise
+
+    p1.catch { _ in
+      calls += 1
+    }
+    p1.flatMapError { _ in p2 }
+      .then { _ in
+        calls += 1
+      }
+
+    source1.reject(NSError(domain: PromissumErrorDomain, code: 42, userInfo: nil))
+    source2.resolve(42)
+
+    XCTAssert(calls == 2, "Calls should be 2")
+  }
+
+  func testValueFlatMap4() {
+    var calls = 0
+
+    let source1 = PromiseSource<Int>()
+    let p1 = source1.promise
+    let source2 = PromiseSource<Int>()
+    let p2 = source2.promise
+
+    p1.catch { _ in
+      calls += 1
+    }
+    p1.flatMapError { _ in p2 }
+      .catch { _ in
+        calls += 1
+      }
+
+    source1.reject(NSError(domain: PromissumErrorDomain, code: 42, userInfo: nil))
+    source2.reject(NSError(domain: PromissumErrorDomain, code: 42, userInfo: nil))
+
+    XCTAssert(calls == 2, "Calls should be 2")
+  }
+
   func testFinally() {
     var calls = 0
 
