08a4f96ef4daf4f29809186b2a3bfd954020f82d
Tom Lokhorst <tom@lokhorst.eu>
2016-09-15 23:36:22 +0200

    Update Alamofire+Promise

--- extensions/PromissumExtensions/Alamofire+Promise.swift
+++ extensions/PromissumExtensions/Alamofire+Promise.swift
@@ -12,12 +12,12 @@ import Alamofire
 /// Used to store all response data returned from a successfully completed `Request`.
 /// Based on `Alamofire.Response`.
 public struct SuccessResponse<Value> {
-  public let request: NSURLRequest?
-  public let response: NSHTTPURLResponse?
-  public let data: NSData?
-  public let result: Value
+  public var request: URLRequest?
+  public var response: HTTPURLResponse?
+  public var data: Data?
+  public var result: Value
 
-  public init(request: NSURLRequest?, response: NSHTTPURLResponse?, data: NSData?, result: Value) {
+  public init(request: URLRequest?, response: HTTPURLResponse?, data: Data?, result: Value) {
     self.request = request
     self.response = response
     self.data = data
@@ -28,13 +28,13 @@ public struct SuccessResponse<Value> {
 
 /// Used to store all response data returned from a failed completed `Request`.
 /// Based on `Alamofire.Response`.
-public struct ErrorResponse<Error: ErrorType> : ErrorType {
-  public let request: NSURLRequest?
-  public let response: NSHTTPURLResponse?
-  public let data: NSData?
-  public let result: Error
+public struct ErrorResponse {
+  public var request: URLRequest?
+  public var response: HTTPURLResponse?
+  public var data: Data?
+  public var result: Error
 
-  public init(request: NSURLRequest?, response: NSHTTPURLResponse?, data: NSData?, result: Error) {
+  public init(request: URLRequest?, response: HTTPURLResponse?, data: Data?, result: Error) {
     self.request = request
     self.response = response
     self.data = data
@@ -42,39 +42,55 @@ public struct ErrorResponse<Error: ErrorType> : ErrorType {
   }
 }
 
-extension Request {
+extension DataRequest {
 
-  public func responsePromise(
-    queue queue: dispatch_queue_t? = nil)
-    -> Promise<SuccessResponse<NSData?>, ErrorResponse<NSError>>
+  public func responsePromise(queue: DispatchQueue? = nil)
+    -> Promise<SuccessResponse<Data?>, ErrorResponse>
   {
-    let source = PromiseSource<SuccessResponse<NSData?>, ErrorResponse<NSError>>()
+    let source = PromiseSource<SuccessResponse<Data?>, ErrorResponse>()
 
-    self.response(queue: queue) { (request, response, data, error) in
-      if let error = error {
-        source.reject(ErrorResponse(request: request, response: response, data: data, result: error))
+    self.response(queue: queue) { response in
+      if let error = response.error {
+        source.reject(ErrorResponse(
+          request: response.request,
+          response: response.response,
+          data: response.data,
+          result: error))
       }
       else {
-        source.resolve(SuccessResponse(request: request, response: response, data: data, result: data))
+        source.resolve(SuccessResponse(
+          request: response.request,
+          response: response.response,
+          data: response.data,
+          result: response.data))
       }
     }
 
     return source.promise
   }
 
-  public func responsePromise<T: ResponseSerializerType>(
-    queue queue: dispatch_queue_t? = nil,
+  public func responsePromise<T: DataResponseSerializerProtocol>(
+    queue: DispatchQueue? = nil,
     responseSerializer: T)
-    -> Promise<SuccessResponse<T.SerializedObject>, ErrorResponse<T.ErrorObject>>
+    -> Promise<SuccessResponse<T.SerializedObject>, ErrorResponse>
   {
-    let source = PromiseSource<SuccessResponse<T.SerializedObject>, ErrorResponse<T.ErrorObject>>()
+    let source = PromiseSource<SuccessResponse<T.SerializedObject>, ErrorResponse>()
 
     self.response(queue: queue, responseSerializer: responseSerializer) { response in
       switch response.result {
-      case .Success(let value):
-        source.resolve(SuccessResponse(request: response.request, response: response.response, data: response.data, result: value))
-      case .Failure(let error):
-        source.reject(ErrorResponse(request: response.request, response: response.response, data: response.data, result: error))
+      case .success(let value):
+        source.resolve(SuccessResponse(
+          request: response.request,
+          response: response.response,
+          data: response.data,
+          result: value))
+
+      case .failure(let error):
+        source.reject(ErrorResponse(
+          request: response.request,
+          response: response.response,
+          data: response.data,
+          result: error))
       }
     }
 
@@ -84,43 +100,43 @@ extension Request {
 
 // MARK: - Data
 
-extension Request {
+extension DataRequest {
   public func responseDataPromise()
-    -> Promise<SuccessResponse<NSData>, ErrorResponse<NSError>>
+    -> Promise<SuccessResponse<Data>, ErrorResponse>
   {
-    return self.responsePromise(responseSerializer: Request.dataResponseSerializer())
+    return self.responsePromise(responseSerializer: DataRequest.dataResponseSerializer())
   }
 }
 
 // MARK: - String
 
-extension Request {
+extension DataRequest {
   public func responseStringPromise(
-    encoding encoding: NSStringEncoding? = nil)
-    -> Promise<SuccessResponse<String>, ErrorResponse<NSError>>
+    encoding: String.Encoding? = nil)
+    -> Promise<SuccessResponse<String>, ErrorResponse>
   {
-    return self.responsePromise(responseSerializer: Request.stringResponseSerializer(encoding: encoding))
+    return self.responsePromise(responseSerializer: DataRequest.stringResponseSerializer(encoding: encoding))
   }
 }
 
 // MARK: - JSON
 
-extension Request {
+extension DataRequest {
   public func responseJSONPromise(
-    options options: NSJSONReadingOptions = .AllowFragments)
-    -> Promise<SuccessResponse<AnyObject>, ErrorResponse<NSError>>
+    options: JSONSerialization.ReadingOptions = .allowFragments)
+    -> Promise<SuccessResponse<Any>, ErrorResponse>
   {
-    return self.responsePromise(responseSerializer: Request.JSONResponseSerializer(options: options))
+    return self.responsePromise(responseSerializer: DataRequest.jsonResponseSerializer(options: options))
   }
 }
 
 // MARK: - Property List
 
-extension Request {
+extension DataRequest {
   public func responsePropertyListPromise(
-    options options: NSPropertyListReadOptions = NSPropertyListReadOptions())
-    -> Promise<SuccessResponse<AnyObject>, ErrorResponse<NSError>>
+    options: PropertyListSerialization.ReadOptions = [])
+    -> Promise<SuccessResponse<Any>, ErrorResponse>
   {
-    return self.responsePromise(responseSerializer: Request.propertyListResponseSerializer(options: options))
+    return self.responsePromise(responseSerializer: DataRequest.propertyListResponseSerializer(options: options))
   }
 }
