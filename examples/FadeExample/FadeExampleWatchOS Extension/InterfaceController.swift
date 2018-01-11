29e29cf83a907489fe2dd0c4607e2d4997feb71b
Tom Lokhorst <tom@lokhorst.eu>
2018-01-12 00:03:10 +0100

    Update watchOS example

--- examples/FadeExample/FadeExampleWatchOS Extension/InterfaceController.swift
+++ examples/FadeExample/FadeExampleWatchOS Extension/InterfaceController.swift
@@ -12,19 +12,41 @@ import Alamofire
 import Promissum
 
 class InterfaceController: WKInterfaceController {
+
+  @IBOutlet var label: WKInterfaceLabel!
+
   override func willActivate() {
     super.willActivate()
+
+    label.setTextColor(UIColor.white)
+    label.setText("...")
     
     let url = "https://api.github.com/repos/tomlokhorst/Promissum"
     
     // Start loading the JSON
     Alamofire.request(url)
       .responseJSONPromise()
-      .then {
-        print($0)
+      .map { self.parse($0.result) }
+      .then { [weak label] p in
+        label?.setTextColor(UIColor.white)
+        label?.setText(p.description)
       }
-      .trap {
-        print($0)
+      .trap { [weak label] error in
+        label?.setTextColor(UIColor.red)
+        label?.setText("\(error)")
       }
   }
+
+  func parse(_ json: Any) -> (name: String, description: String) {
+
+    let dict = json as! [String: Any]
+    let name = dict["name"] as! String
+    let description = dict["description"] as! String
+
+    return (name, description)
+  }
+}
+
+struct Project: Decodable {
+  let description: String
 }
