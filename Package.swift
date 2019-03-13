99780686ec2f7e756fcf5aa80c03a024c3ed21ba
Mathijs Bernson <mathijsb@q42.nl>
2019-03-13 15:54:52 +0100

    Update Swift package definition

--- Package.swift
+++ Package.swift
@@ -3,9 +3,13 @@ import PackageDescription
 
 let package = Package(
   name: "Promissum",
+  products: [
+    .library(name: "Promissum", targets: ["Promissum"]),
+  ],
   dependencies: [],
   targets: [
     .target(name: "Promissum"),
+    .testTarget(name: "PromissumTests", dependencies: ["Promissum"]),
   ]
 )
 
