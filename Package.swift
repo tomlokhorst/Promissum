464ce32fdbd02659815d685f23affc25d6958410
Tom Lokhorst <tom@lokhorst.eu>
2019-06-10 16:41:36 +0200

    Test without platforms

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 
 let package = Package(
   name: "Promissum",
-  platforms: [
-    .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
-  ],
+//  platforms: [
+//    .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
+//  ],
   products: [
     .library(name: "Promissum", targets: ["Promissum"]),
     .library(name: "PromissumAlamofire", targets: ["PromissumAlamofire"]),
